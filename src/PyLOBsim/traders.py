c277f3d4c7f2c3f84a449ed3c05aa11e5cc5a9dd
Ash Booth <ash.booth@soton.ac.uk>
2013-04-15 21:34:53 +0100

    All trader types outlined

--- src/PyLOBsim/traders.py
+++ src/PyLOBsim/traders.py
@@ -4,7 +4,6 @@ Created on 15 Apr 2013
 @author: Ash Booth
 
 TODO:
-- bookeep is nonsense at the moment!
 
 
 '''
@@ -18,29 +17,27 @@ class Trader(object):
     - list of orders to execute
     '''
 
-
     def __init__(self, ttype, tid, balance):
         self.ttype = ttype
         self.tid = tid
         self.balance = balance
         self.blotter = []
-        self.orders = []
+        self.outstandingOrders = []
         self.willing = 1
         self.able = 1
         self.lastquote = None
 
-    def bookkeep(self, trade, order, verbose):
+    def bookkeep(self, trade, side, verbose):
         self.blotter.append(trade)  # add trade record to trader's blotter
         # NB What follows is **LAZY** -- assumes all orders are quantity=1
-        transactionprice = trade['price']
-        if self.orders[0].otype == 'Bid':
-                profit = self.orders[0].price - transactionprice
+        transactionPrice = trade['price']
+        prevBalance = self.balance
+        if side == 'bid':
+            self.balance -= transactionPrice
         else:
-                profit = transactionprice - self.orders[0].price
-        self.balance += profit
-        if verbose: print('%s profit=%d balance=%d ' % 
-                          (outstr, profit, self.balance))
-        self.del_order(order)  # delete the order
+            self.balance += transactionPrice
+        if verbose: print('previous balance=%f, new balance=%f ' % 
+                          (prevBalance, self.balance))
         
     def respond(self, time, lob, trade, verbose):
         '''
@@ -52,5 +49,65 @@ class Trader(object):
 
     def __str__(self):
         return ('[TID %s type %s balance %s blotter %s orders %s]' % 
-                (self.tid, self.ttype, self.balance, self.blotter, self.orders))
+                (self.tid, self.ttype, 
+                 self.balance, self.blotter, 
+                 self.outstandingOrders))
+        
+        
+class MarketMaker(Trader):
+    '''
+    Intermediaries, that post prices on both sides of the order book and
+    try to maintain their position throughout the day, making their income from
+    the difference between their bid and offer price.
+    '''
+    
+    def getOrder(self):
+        pass
+    
+
+class HFT(Trader):
+    '''
+    High Frequency Traders, who have a relatively low net position throughout
+    the day, compared to their activity. They are similar MarketMakers, but
+    have much higher trading activity and much shorter holding periods.
+    '''
+    
+    def getOrder(self):
+        pass
+    
+
+class FBuyers(Trader):
+    '''
+    Fundamental Buyers, who try to build a long position during the day.
+    '''
+    
+    def getOrder(self):
+        pass
+
+
+class FSellers(Trader):
+    '''
+    Fundamental Sellers, who try to build a short position during the day.
+    '''
+    
+    def getOrder(self):
+        pass
+    
+    
+class Opportunistic(Trader):
+    '''
+    Opportunistic Traders, who may behave as intermediaries at times, or as
+    fundamental traders at times when they see significant directional moves.
+    '''
+    
+    def getOrder(self):
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
         
