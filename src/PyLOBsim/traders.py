7c548c22ab8c1fd4e01e8bff69866b9e3a61bb4b
Ash Booth <ash.booth@soton.ac.uk>
2013-04-15 17:26:27 +0100

    First real commit. Couple of template files added

---
+++ src/PyLOBsim/traders.py
@@ -0,0 +1,17 @@
+'''
+Created on 15 Apr 2013
+
+@author: Ash Booth
+'''
+
+class Trader(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ src/main.py
@@ -0,0 +1,11 @@
+'''
+Created on 15 Apr 2013
+
+@author: Ash Booth
+'''
+
+if __name__ == '__main__':
+    from PyLOB import OrderBook
+    
+    lob = OrderBook()
+    print "success!"
